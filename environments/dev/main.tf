8616355658aa8ef419a48e7f5c096c342d26c04f
Abhilash Chandel <abhilashchandel74@gmail.com>
2026-08-09 17:10:47 +0530

    updated code

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -1,11 +1,11 @@
 module "resource_group" {
-  source = "../../Modules/resource_group"
-  rg = var.rg
+  source = "../../modules/resource_group"
+  rg     = var.rg
 }
 
 module "storage_account" {
-  source = "../../modules/storage_account"
+  source          = "../../modules/storage_account"
   storage_Account = var.storage_Account
-  rg_location = module.resource_group.rg_location
-  rg_name = module.resource_group.rg_name
+  rg_location     = module.resource_group.rg_location
+  rg_name         = module.resource_group.rg_name
 }
