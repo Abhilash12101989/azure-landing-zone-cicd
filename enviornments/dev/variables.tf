2dcc41a2c8d9800fc1a4f378c35031a05d33fb0b
Abhilash Chandel <abhilashchandel74@gmail.com>
2026-08-14 21:32:56 +0530

    Remove unused storage account variable from dev environment

--- enviornments/dev/variables.tf
+++ enviornments/dev/variables.tf
@@ -5,11 +5,3 @@ variable "rg" {
   }))
 }
 
-variable "storage_Account" {
-  type = map(object({
-    name                     = string
-    rg_key                   = string
-    account_tier             = string
-    account_replication_type = string
-  }))
-}
