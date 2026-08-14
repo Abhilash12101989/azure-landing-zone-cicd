fd5681831129944701faa085c81efa0902aeab1b
Abhilash Chandel <abhilashchandel74@gmail.com>
2026-08-14 19:33:01 +0530

    Remove storage account module and add Terraform variables for resource group configuration

--- enviornments/dev/main.tf
+++ enviornments/dev/main.tf
@@ -3,9 +3,4 @@ module "resource_group" {
   rg = var.rg
 }
 
-module "storage_account" {
-  source = "../../modules/storage_account"
-  storage_Account = var.storage_Account
-  rg_location = module.resource_group.rg_location
-  rg_name = module.resource_group.rg_name
-}
+
