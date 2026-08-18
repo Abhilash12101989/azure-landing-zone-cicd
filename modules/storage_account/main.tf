e27ffa8f6cdaa3b4d8eea26b89e645bec1987a5a
Abhilash Chandel <abhilashchandel74@gmail.com>
2026-08-18 12:47:03 +0530

    code updated

--- modules/storage_account/main.tf
+++ modules/storage_account/main.tf
@@ -6,5 +6,8 @@ resource "azurerm_storage_account" "storage_account" {
   account_tier             = each.value.account_tier
   account_replication_type = each.value.account_replication_type
 
+   # checkov:skip=CKV_AZURE_59:Terraform backend is accessed by GitHub-hosted runners; public network access is required until private endpoint connectivity is implemented.
+   # checkov:skip=CKV_AZURE_33:Queue service is not used; this storage account is dedicated to Terraform state stored in Azure Blob Storage.
+
   tags = var.common_tags
 }
