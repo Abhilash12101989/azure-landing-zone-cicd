fb7210b1f5833e2f75d3f6f9e109b3efbabd7704
Abhilash Chandel <99348737+Abhilash12101989@users.noreply.github.com>
2026-08-19 00:17:14 +0530

    Feature/vnet (#8)
    * Remove storage account module and add Terraform variables for resource group configuration
    
    * Add feature branch workflow and remove storage account module
    
    - Created a new GitHub Actions workflow for feature branches.
    - Updated .gitignore to include Terraform lock files.
    - Refactored Terraform resource group module for consistency.
    - Removed storage account module and its associated variables.
    
    * Remove main branch trigger from workflow and fix module path casing in Terraform configuration
    
    * Fix indentation for resource group variable and module configuration
    
    * Add Azure login step to feature branch workflow
    
    * Add Terraform Plan step to feature branch workflow
    
    * Remove unused storage account variable from dev environment
    
    * Add step to upload Terraform plan artifact in feature branch workflow
    
    * Update Terraform version to 1.14.3 in feature branch workflow
    
    * Add branch specification for feature branch workflow trigger
    
    * Fix directory name typos and restore Terraform configuration files in dev environment
    
    * Refactor main workflow to separate plan and apply jobs for Terraform
    
    * Add TFlint setup and update required Terraform version in configuration
    
    * Implement resource group and storage account modules with common tags; update Terraform configurations and variables
    
    * Format code for consistency in Terraform files across modules
    
    * code updated
    
    * Add permissions section to workflows and enhance storage account configurations
    
    - Updated feature.yml and main.yml to include permissions: read-all
    - Enhanced azurerm_storage_account resource with min_tls_version and public access settings
    - Added blob_properties and sas_policy configurations to storage account
    - Included checkov skip comments for storage container resource

--- modules/storage_container/main.tf
+++ modules/storage_container/main.tf
@@ -2,4 +2,6 @@ resource "azurerm_storage_container" "example" {
   for_each           = var.container
   name               = each.value.name
   storage_account_id = var.storage_accountid[each.value.storage_key]
+
+  # checkov:skip=CKV2_AZURE_21:Storage logging for Blob service read requests is not configured for state storage containers.
 }
